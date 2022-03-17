5ce701eaf62203dfd54a870d02d769d56aafe016
Guillaume Anthouard <guillaume.anthouard@hotmail.fr>
2022-03-17 17:02:54 +0100

    Add random media endpoints

--- src/app.js
+++ src/app.js
@@ -3,18 +3,24 @@ import morgan from 'morgan';
 import cors from 'cors';
 import { initializeDBConn } from './lib/db/conn.js';
 import mediaRouter from './lib/medias.js';
+import homeRouter from './lib/home.js';
 
 const app = express();
 
+// CORS middleware
 app.use(cors());
 
+// Call logger
 if (process.env.NODE_ENV !== 'production') app.use(morgan('combined'));
 
 app.get('/', async (req, res) => {
   res.send('Hello !');
 });
 
-// Register items endpoint
+// Register homepage endpoint
+app.use('/home', homeRouter);
+
+// Register medias endpoints
 app.use('/medias', mediaRouter);
 
 // Connect to database and start web server on success
