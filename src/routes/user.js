c8ca1c4b3eeb955366b89ffed4c0e2264cb9c2ff
Lufou <lucas.damase@laposte.net>
2022-05-15 13:20:53 +0200

    Create profile endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import { userColl } from '../db/conn.js';
 import { idFilter } from '../db/bson.js';
-import { requireAuth, validateEmail } from '../auth.js';
+import { requireAuth } from '../auth.js';
+import { validateURL, validateEmail } from '../utils.js';
 
 const userRouter = express.Router();
 export default userRouter;
@@ -139,4 +140,77 @@ userRouter.post('/deleteUserProfile', async (req, res) => {
   };
   await userColl.updateOne(query, updateDocument);
   res.send({ response: 'OK' });
+});
+
+/**
+ * @openapi
+ *
+ * /me/createUserProfile:
+ *   post:
+ *     summary: "Create user profile"
+ *     operationId: user.profile.create
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                  type: string
+ *               picture:
+ *                  type: string
+ *             required:
+ *               - authToken
+ *               - name
+ *               - picture
+ *     responses:
+ *       "200":
+ *         description: "The profile has been created."
+ *       default:
+ *         $ref: "#/components/responses/default"
+ *     security:
+ *       - token: []
+ */
+userRouter.post('/createUserProfile', async (req, res) => {
+  const { profileName, profilePicture } = req.body;
+  if (profileName === '') {
+    res.send({ error: 'InvalidNameError' });
+    return;
+  }
+  if (!validateURL(profilePicture)) {
+    res.send({ error: 'InvalidUrlError' });
+    return;
+  }
+  const newProfileId = userCache[req.auth.id].profiles.length;
+  userCache[req.auth.id].profiles[newProfileId] = {};
+  userCache[req.auth.id].profiles[newProfileId].name = '';
+  userCache[req.auth.id].profiles[newProfileId].email = '';
+  userCache[req.auth.id].profiles[newProfileId].picture = profilePicture;
+
+  const query = idFilter(req.auth.id);
+  let setValue = {};
+  setValue[`profiles.${newProfileId}`] = [];
+  let updateDocument = {
+    $set: setValue,
+  };
+  await userColl.updateOne(query, updateDocument);
+  setValue = {};
+  setValue[`profiles.${newProfileId}.0`] = profileName;
+  updateDocument = {
+    $set: setValue,
+  };
+  await userColl.updateOne(query, updateDocument);
+  setValue = {};
+  setValue[`profiles.${newProfileId}.1`] = '';
+  updateDocument = {
+    $set: setValue,
+  };
+  await userColl.updateOne(query, updateDocument);
+  setValue = {};
+  setValue[`profiles.${newProfileId}.2`] = profilePicture;
+  updateDocument = {
+    $set: setValue,
+  };
+  await userColl.updateOne(query, updateDocument);
+  res.send({ response: newProfileId });
 });
